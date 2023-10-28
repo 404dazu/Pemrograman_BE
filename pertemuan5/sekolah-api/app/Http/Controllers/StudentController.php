cde7fa10acaaebe9bf44b37a6a62be7d8f65cbf8
404dazu <daffazuhair77@gmail.com>
2023-10-28 09:26:30 +0700

    Added "Tugas Pertemuan ke 5"

---
+++ pertemuan5/sekolah-api/app/Http/Controllers/StudentController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Student;
+
+class StudentController extends Controller
+{
+    public function index(){
+        $students = Student::all();
+        $data = [
+            'message' => 'GET ALL STUDENTS',
+            'data' => $students
+        ];
+
+        return response()->json($data,200);
+    } 
+
+    public function store(Request $request){
+        $input = [
+            'nama'=>$request->nama,
+            'nim'=>$request->nim,
+            'email'=>$request->email,
+            'jurusan'=>$request->jurusan
+        ];
+
+        $students=Student::create($input);
+        $data=[
+            'message' => 'Student has been created succesfully',
+            'data' => $student,
+        ];
+
+        return response()->json($data, 201);
+    }
+}
---
+++ pertemuan5/sekolah-api/app/Student.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Student extends Model
+{
+    use HasFactory;
+}
