924130eed5c80e945b1b5cf0a2adb062771c6f0a
404dazu <daffazuhair77@gmail.com>
2023-11-22 18:13:58 +0700

    Added "UTS"

--- UTS/covid-api/app/Http/Controllers/AuthController.php
+++ UTS/covid-api/app/Http/Controllers/AuthController.php
@@ -2,9 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    // membuat akun authentikasi.
+    public function register(Request $request){
+        $input=[
+            'nama'=>$request->nama,
+            'email'=>$request->email,
+            'password'=>Hash::make($request->password),
+        ];
+        $user=User::create($input);
+        $data=[
+            'message' => 'Your account has been added'
+        ];
+        return response()->json($data, 200);
+    }
+
+    public function login(Request $request){
+        $input = [
+            'email'=>$request->email,
+            'password'=>$request->password,
+        ];
+        
+        $user = User::where('email',$input['email'])->first();
+
+        $isLoginSuccesfully=(
+            $input['email']==$user->email
+            &&
+            Hash::check($input['password'], $user->password)
+        );
+        if($isLoginSuccesfully){
+            $token=$user->createToken('auth_token');
+
+            $data=[
+                'message' => 'Login is Succesfully',
+                'token'=>$token->plainTextToken,
+            ];
+            return response()->json($data,200);
+        }else{
+            $data=[
+                'message'=>'Username or Password is wrong!'
+            ];
+
+            return response()->json($data,401);
+        }
+    }
 }
